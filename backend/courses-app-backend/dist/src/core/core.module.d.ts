cfeaf78467b62cd16e59fb88619f9e803cc6b7fd
Karol Sawicki <karol_sawicki@epam.com>
2021-12-07 11:25:59 +0100

    fix: remove backend

--- backend/courses-app-backend/dist/src/core/core.module.d.ts
+++
@@ -1,5 +0,0 @@
-import { DynamicModule } from '@nestjs/common';
-import { CoreModuleConfig } from './core-module.config';
-export declare class CoreModule {
-    static forRoot({ algorithm, timeLiving, filesFolder, }: CoreModuleConfig): DynamicModule;
-}
